12c0f1bcf817cb4f64f075fa2201c1364509301f
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-17 01:17:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Justin."
+  end
+  
+  get '/hometown' do 
+    "My hometown is Carroll."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Circle - Post Malone."
+  end
+  
 end
