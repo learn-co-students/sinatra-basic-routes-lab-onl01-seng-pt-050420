0c06154ddd2a003c045c970a4e09ea48e33450a8
Jonathan East <jonathan_east@ymail.com>
2020-07-18 20:21:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Jonny."
+  end
+
+  get "/hometown" do
+    "My hometown is Martinsburg."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is After Dark by 'Mr. Kitty'"
+  end
 end
