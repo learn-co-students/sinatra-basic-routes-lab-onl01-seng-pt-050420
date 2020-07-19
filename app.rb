2004a9fbc05d5f84ab1a57aee203e055fe4418e2
C0mbed <matt@farncombe.co>
2020-07-19 11:22:36 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Matt Farncombe."
+    end 
+
+    get '/hometown' do
+        "My hometown is Oakville, Canada"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Welcome to the Jungle"
+    end
 end
