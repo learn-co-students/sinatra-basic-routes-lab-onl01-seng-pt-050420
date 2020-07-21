f0aa134c736d32facad3029425c3f424624303f9
Kevin Boston <kbos22@gmail.com>
2020-07-21 13:35:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end
+  get '/name' do 
+    "My name is Kevin"
+  end
+  get '/hometown' do 
+    "My hometown is Manassas"
+  end
+  get '/favorite-song' do 
+    "My favorite song is The Sound of Silence"
+  end
+  
 end
