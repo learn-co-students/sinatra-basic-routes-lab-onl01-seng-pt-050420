50992481b84f5d2a9122af906ddc06d83915464c
ValentinaPanic <valentina.panic87@gmail.com>
2020-07-21 18:58:26 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+ 
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Valentina Panic."
+    end
+    get '/hometown' do
+        "My hometown is Novi Sad."
+    end
+    get '/favorite-song' do
+        "My favorite song is Memories from Gentleman."
+    end
+
 end
