330846330fdcb583753aadc2095811cfd78c747e
Billy Ferrara <willfira717@gmail.com>
2020-07-27 20:00:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Billy"
+  end
+
+  get '/hometown' do
+    "My hometown is Sayville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Never Gonna Give You Up"
+  end 
 end
