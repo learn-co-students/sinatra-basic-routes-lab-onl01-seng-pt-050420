bbf5cf61dcc5885930e53954f64febe39f85826f
“VictorW77-ios” <victorwilliams1030@gmail.com>
2020-07-30 11:28:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Victor"
+    end 
+    
+    get '/hometown' do 
+        "My hometown is Ruston, LA"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Let Me Know by Brent Faiyaz"
+    end
 end
