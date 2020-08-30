811572e93b44d7eb09e6e2a80a4777a018952ac8
Jennifer Prince <jennifergraceprince@gmail.com>
2020-08-30 04:25:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Jennifer"
+  end
+  
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Temptation by New Order"
+  end
 end
